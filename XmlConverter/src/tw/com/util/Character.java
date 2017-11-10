319420842d6de7e711a3bca474b01a524e7706b0
Ian <qaz7456@gmail.com>
2017-11-10 18:58:28 +0800

    all

--- XmlConverter/src/tw/com/util/Character.java
+++ XmlConverter/src/tw/com/util/Character.java
@@ -7,6 +7,8 @@ import org.json.JSONObject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import sun.util.logging.resources.logging;
+
 public class Character {
 
 	public static String null2str(Object object) {
@@ -21,35 +23,46 @@ public class Character {
 	 *            a string that might be XML
 	 * @return true of the string is XML, false otherwise
 	 */
+//	public static boolean isXMLLike(String inXMLStr) {
+//
+//		boolean retBool = false;
+//		Pattern pattern;
+//		Matcher matcher;
+//
+//		// REGULAR EXPRESSION TO SEE IF IT AT LEAST STARTS AND ENDS
+//		// WITH THE SAME ELEMENT
+//		final String XML_PATTERN_STR = "<(\\S+?)(.*?)>(.*?)</\\1>";
+//
+//		// IF WE HAVE A STRING
+//		if (inXMLStr != null && inXMLStr.trim().length() > 0) {
+//
+//			// IF WE EVEN RESEMBLE XML
+//			if (inXMLStr.trim().startsWith("<")) {
+//
+//				pattern = Pattern.compile(XML_PATTERN_STR,
+//						Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+//
+//				// RETURN TRUE IF IT HAS PASSED BOTH TESTS
+//				matcher = pattern.matcher(inXMLStr);
+//				retBool = matcher.matches();
+//			}
+//			// ELSE WE ARE FALSE
+//		}
+//
+//		return retBool;
+//	}
+
 	public static boolean isXMLLike(String inXMLStr) {
 
 		boolean retBool = false;
-		Pattern pattern;
-		Matcher matcher;
-
-		// REGULAR EXPRESSION TO SEE IF IT AT LEAST STARTS AND ENDS
-		// WITH THE SAME ELEMENT
-		final String XML_PATTERN_STR = "<(\\S+?)(.*?)>(.*?)</\\1>";
-
-		// IF WE HAVE A STRING
-		if (inXMLStr != null && inXMLStr.trim().length() > 0) {
-
-			// IF WE EVEN RESEMBLE XML
-			if (inXMLStr.trim().startsWith("<")) {
-
-				pattern = Pattern.compile(XML_PATTERN_STR,
-						Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
-
-				// RETURN TRUE IF IT HAS PASSED BOTH TESTS
-				matcher = pattern.matcher(inXMLStr);
-				retBool = matcher.matches();
-			}
-			// ELSE WE ARE FALSE
+		try {
+			XMLConverter.getDocument(inXMLStr);
+			retBool = true;
+		} catch (Exception e) {
+			return retBool;
 		}
-
 		return retBool;
 	}
-
 	public static boolean isJSONValid(String jsonInString) {
 		try {
 			new JSONObject(jsonInString);
--- XmlConverter/src/tw/com/util/XMLConverter.java
+++ XmlConverter/src/tw/com/util/XMLConverter.java
@@ -1,9 +1,15 @@
 package tw.com.util;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +24,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
@@ -30,17 +37,32 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import com.google.gson.Gson;
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 public class XMLConverter {
 	private static final Logger logger = LogManager.getLogger(XMLConverter.class);
+	public static String FILE_PATH = null;
 
 	public static void main(String[] args) throws Exception {
 		String xml = "<Request service=\"SALE_ORDER_STATUS_PUSH_SERVICE\" lang=\"zh-CN\"><Body><SaleOrderStatusRequest><CompanyCode>W8860571504</CompanyCode><SaleOrders><SaleOrder><WarehouseCode>886DCA</WarehouseCode><ErpOrder>20170803TW1</ErpOrder><WayBillNo>289081343391</WayBillNo><ShipmentId>OXMS201708030114140703</ShipmentId><Waves>EW886A17080300102</Waves><CartNum>1</CartNum><GridNum>0001</GridNum><Carrier>顺丰速运</Carrier><CarrierProduct>島内件(80CM0.5-1.5KG)</CarrierProduct><IsSplit>N</IsSplit><Steps><Step><EventTime>2017-08-0315:31:25</EventTime><EventAddress>WOM</EventAddress><Status>1400</Status><Note>您的订单已取消,取消原因：客户要求取消订单</Note></Step></Steps></SaleOrder></SaleOrders></SaleOrderStatusRequest></Body></Request>";
 		String json = "{\"Request\":{\"_lang\":\"zh-CN\",\"_service\":\"SALE_ORDER_STATUS_PUSH_SERVICE\",\"Body\":{\"SaleOrderStatusRequest\":{\"SaleOrders\":{\"SaleOrder\":{\"ShipmentId\":\"OXMS201708030114140703\",\"Sender\":\"顺丰速运\",\"Steps\":{\"Step\":{\"Status\":1400,\"EventTime\":\"2017-08-0315:31:25\",\"Note\":\"您的订单已取消,取消原因：客户要求取消订单\",\"EventAddress\":\"WOM\"}},\"ErpNo\":\"20170803TW1\",\"CartNum\":1,\"WayBillNo\":289081343391,\"GridNum\":\"0001\",\"Product\":\"島内件(80CM0.5-1.5KG)\",\"IsSplit\":\"N\",\"WarehouseCode\":\"886DCA\",\"Waves\":\"EW886A17080300102\"}},\"Code\":\"W8860571504\"}}}}";
-		logger.debug(getJson(xml));
-		logger.debug(getXml(json));
+		// xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response
+		// Status=\"ok\"><Product><ProductId>p09498618</ProductId><ProductStatus>Online</ProductStatus><ProductName>清涼櫻花剉冰111</ProductName><ShortDescription>白玉櫻花剉冰~清涼退火又消暑</ShortDescription><spec
+		// Id=\"1\"><SpecDescription>-</SpecDescription><CustomizedProductId/><CurrentStock>300</CurrentStock><Stock>1</Stock></spec><ImageMain>https://b-iu101.ac.tw1.yahoo.com/pimg1/c0/05/p09498618-itema-1991xf1x0300x0183-s.jpg</ImageMain><MarketPrice>0</MarketPrice><SalePrice>200</SalePrice><CostPrice>100</CostPrice></Product></Response>";
+		String xmlConverterPath = "C:\\Users\\Ian\\Desktop\\Development\\xmlconverter-config.xml";
+		xml="<?xml version=\"1.0\" encoding=\"gbk\" standalone=\"no\"?><Request lang=\"zh-CN\" service=\"SALE_ORDER_STATUS_PUSH_SERVICE\"><Body><SaleOrderStatusRequest><CompanyCode>W8860571504</CompanyCode><SaleOrders><SaleOrder><WarehouseCode>886DCA</WarehouseCode><ErpOrder>20170803TW1</ErpOrder><WayBillNo>289081343391</WayBillNo><ShipmentId>OXMS201708030114140703</ShipmentId><Waves>EW886A17080300102</Waves><CartNum>1</CartNum><GridNum>0001</GridNum><Carrier>顺丰速运</Carrier><CarrierProduct>島内件(80CM 0.5-1.5KG)</CarrierProduct><IsSplit>N</IsSplit><Steps><Step><EventTime>2017-08-03 15:31:25</EventTime><EventAddress>WOM</EventAddress><Status>1400</Status><Note>您的订单已取消,取消原因：客户要求取消订单</Note></Step></Steps></SaleOrder></SaleOrders></SaleOrderStatusRequest></Body></Request>";
+		// logger.debug(getJson(xml));
+		// logger.debug(getXml(xml,xmlConverterPath));
+		// logger.debug(getRest(xml, xmlConverterPath));
+		System.out.println();
+		logger.debug("得到: " + getRest("Get", xml, xmlConverterPath));
+		System.out.println();
+
+		// String xmlUrl = args[0];
 	}
 
-	private static List<Map<String, String>> getConverterConfigList() throws Exception {
+	private static List<Map<String, String>> getConverterConfigList(String path) throws Exception {
 		System.out.println();
 		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
 		Map<String, String> map = null;
@@ -52,11 +74,14 @@ public class XMLConverter {
 		} catch (ParserConfigurationException e) {
 			logger.error(e);
 		}
-		InputStream is = null;
-		is = XMLConverter.class.getResourceAsStream("/resources/converter-config.xml");
+		// InputStream is = null;
+
+		// is = new FileInputStream(path);
+		logger.debug("XMLConverter path: {}", path);
+		File file = new File(path);
 		Document doc = null;
 		try {
-			doc = dombuilder.parse(is);
+			doc = dombuilder.parse(file);
 		} catch (SAXException | IOException e) {
 			logger.error(e);
 		}
@@ -65,7 +90,6 @@ public class XMLConverter {
 
 		NodeList fields = root.getChildNodes();
 
-		logger.debug("getConverterConfigMap");
 		for (int i = 0; i < fields.getLength(); i++) {
 			Node node = (Node) fields.item(i);
 			if (node.getNodeType() == Node.ELEMENT_NODE) {
@@ -77,8 +101,6 @@ public class XMLConverter {
 						String nodeName = setting.getNodeName();
 						String textContent = setting.getTextContent();
 						map.put(nodeName, textContent);
-
-						logger.debug("{}: {}", nodeName, textContent);
 					}
 				}
 				list.add(map);
@@ -87,20 +109,115 @@ public class XMLConverter {
 		return list;
 	}
 
-	public static String getXml(String input) throws Exception {
+	public static String trim(String input) {
+		BufferedReader reader = new BufferedReader(new StringReader(input));
+		StringBuffer result = new StringBuffer();
+		try {
+			String line;
+			while ((line = reader.readLine()) != null)
+				result.append(line.trim());
+			return result.toString();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String getRest(String type, String input, String path, String encode) throws Exception {
+
+		String xml = getXml(input, path);
+		logger.debug("轉換後的XML: " + xml);
+		logger.debug("去除空白: " + trim(xml));
+		xml = trim(xml);
+		Map<String, String> map = getXmlMap(xml);
+		StringBuffer sf = new StringBuffer();
+		sf.append("Format=").append(type);
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+			sf.append(key).append("=").append(value).append("&");
+		}
+		sf.setLength(sf.length() - 1);
+
+		String result = null;
+		if ("utf-8".equalsIgnoreCase(encode)) {
+			result = new String(sf.toString().getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+		}
+		if ("Base64".equalsIgnoreCase(encode)) {
+			result = new String(Base64.encodeBase64String(sf.toString().getBytes()));
+		}
+		return result;
+
+	}
+
+	public static String getRest(String type, String input, String path) throws Exception {
+
+		String xml = getXml(input, path);
+		logger.debug("轉換後的XML: " + xml);
+		logger.debug("去除空白: " + trim(xml));
+		Map<String, String> map = getXmlMap(xml);
+		StringBuffer sf = new StringBuffer();
+		sf.append("Format=").append(type).append("&");
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+			sf.append(key).append("=").append(value).append("&");
+		}
+		sf.setLength(sf.length() - 1);
+		return sf.toString();
+
+	}
+
+	public static Map<String, String> getXmlMap(String xml) throws Exception {
+
+		InputStream is = new ByteArrayInputStream(xml.getBytes());
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document document = db.parse(is);
+
+		return createMap(document);
+
+	}
+
+	public static Map<String, String> createMap(Node node) {
+		Map<String, String> map = new HashMap<String, String>();
+		NodeList nodeList = node.getChildNodes();
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node currentNode = nodeList.item(i);
+			if (currentNode.hasAttributes()) {
+				for (int j = 0; j < currentNode.getAttributes().getLength(); j++) {
+					Node item = currentNode.getAttributes().item(j);
+					map.put(item.getNodeName(), item.getTextContent());
+				}
+			}
+			if (node.getFirstChild() != null && node.getFirstChild().getNodeType() == Node.ELEMENT_NODE) {
+				map.putAll(createMap(currentNode));
+			} else if (node.getFirstChild().getNodeType() == Node.TEXT_NODE) {
+				map.put(node.getLocalName(), node.getTextContent());
+			}
+		}
+		return map;
+	}
+
+	public static String getXml(String input, String path) throws Exception {
 		String xml = "";
 		List<String> list = null;
 		if (Character.isJSONValid(input)) {
 			JSONObject json = new JSONObject(input);
 			xml = XML.toString(json);
 			list = new ArrayList<String>();
+			logger.debug("判斷是json型態");
+		} else {
+			logger.debug("判斷不是json型態");
 		}
 		if (Character.isXMLLike(input)) {
 			xml = input;
-
+			logger.debug("判斷是XML型態");
+		} else {
+			logger.debug("判斷不是XML型態");
 		}
 		if (!"".equals(xml)) {
-			List<Map<String, String>> converterConfigList = getConverterConfigList();
+			List<Map<String, String>> converterConfigList = getConverterConfigList(path);
 
 			Document doc = getDocument(xml);
 
@@ -162,17 +279,17 @@ public class XMLConverter {
 		return xml;
 	}
 
-	public static String getJson(String input) throws Exception {
+	public static String getJson(String input, String path) throws Exception {
 
 		String xml = "";
 		if (Character.isJSONValid(input)) {
-			xml = getXml(input);
+			xml = getXml(input, xml);
 		} else if (Character.isXMLLike(input)) {
 			xml = input;
 		}
 		if (!"".equals(xml)) {
 
-			List<Map<String, String>> converterConfigList = getConverterConfigList();
+			List<Map<String, String>> converterConfigList = getConverterConfigList(path);
 
 			Document doc = getDocument(xml);
 
@@ -262,7 +379,7 @@ public class XMLConverter {
 			Transformer transformer = tf.newTransformer();
 			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
 			transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty(OutputKeys.INDENT, "no");
 			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
 
 			transformer.transform(new DOMSource(doc), new StreamResult(sw));
